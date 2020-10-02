5614c1d055077ceb1022160318ec0e83a40ae783
TrungVu <trungvu1706@gmail.com>
2020-10-03 00:44:03 +0700

    add die method

--- mouse.js
+++ mouse.js
@@ -1,10 +1,10 @@
 function Mouse(name) {
     this.name = name;
-    this.dead = false
+    this.dead = false;
 }
 
 Mouse.prototype.die = function() {
     this.dead = true;
-}
+};
 
 module.exports = Mouse;
